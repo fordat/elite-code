05a1311817b59716c5f9c214b9318c94e18faf02
fordat <fordatwater@gmail.com>
2024-06-01 00:04:24 -0700

    partial integer soln

--- integer-to-roman.js
+++ integer-to-roman.js
@@ -3,7 +3,34 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    return num;
+  let result = "";
+  let newNum;
+
+  // M calculation
+
+  mCount = Math.floor(num / 1000);
+  mRemainder = num % 1000;
+
+  for (let i = 0; i < mCount; i++) {
+    result += "M";
+  }
+  newNum = num - mCount * 1000;
+
+  if (mRemainder > 900) {
+    result = result.slice(0, result.length - 1) + "CM";
+    newNum = newNum - 900;
+  }
+
+  console.log("remaining num: ." , newNum);
+  
+
+
+  console.log("mcount: ", mCount, "\n mremainder: ", mRemainder);
+
+  console.log("result: ", result);
+
+  return num;
+
 };
 
-console.log(intToRoman(2));
+console.log(intToRoman(3915));
