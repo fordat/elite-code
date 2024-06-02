964b40ed24d4d601a6de4e33864523f8b84df29c
fordat <fordatwater@gmail.com>
2024-06-01 18:00:20 -0700

    easy one solved

--- is-palindrome.js
+++ is-palindrome.js
@@ -3,8 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-  return x;    
+  let xAsString = x.toString();
+
+  let result = true;
+
+  for (let i = 0; i < xAsString.length; i++) {
+    if (xAsString[i] !== xAsString[xAsString.length - 1 - i]) {
+      result = false;
+    }
+  }
+
+  return result;    
 };
+
+// note: a better solution is to simply reverse the string and see if they are equal.
 
 let num1 = 121;
 let num2 = -121;
