dd28a956193473933929d9671b49253fc7230dac
fordat <fordatwater@gmail.com>
2024-06-03 20:26:49 -0700

    init 3sum

---
+++ three-sum.js
@@ -0,0 +1,13 @@
+// notes and work
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var threeSum = function(nums) {
+
+}
+
+let nums = [-3,-3,4,1,2,0];
+
+console.log(threeSum(nums));
